c180e8ce40c8e0c2a7700d17f7853380e6fc92b1
NonNoname1201 <dobrohorskyi.v@gmail.com>
2024-11-30 15:39:09 +0100

    feat: addition of image related models

--- backend/Back/Models/ImageContainer.cs
+++ backend/Back/Models/ImageContainer.cs
@@ -1,8 +1,8 @@
 ﻿namespace Back.Models;
 
-public class ImageContainer(int id, string mainImage, string[] images)
+public class ImageContainer(int id, Image mainImage, List<Image> images)
 {
     public int Id { get; set; } = id; // To be used as a reference
-    string MainImage { get; set; } = mainImage; // Image that will be displayed in the feed
-    string[] Images { get; set; } = images; // Includes the main image
+    public Image MainImage { get; set; } = mainImage; // Image that will be displayed in the feed
+    public List<Image> Images { get; set; } = images; // Includes the main image
 }
