f6ff45cfe675123bda56c1e1f53936eb0b44da6a
NonNoname1201 <dobrohorskyi.v@gmail.com>
2024-11-30 15:55:34 +0100

    feat: creation of dto models for filtered data

---
+++ backend/Back/Models/DTO/PostDto.cs
@@ -0,0 +1,37 @@
+﻿using Back.Models;
+
+namespace Back.Models.DTO;
+
+public class PostDto
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public UserPostDto Author { get; set; }
+    public string MainImageFilePath { get; set; }
+    public long Likes { get; set; }
+    public string CreatedAt { get; set; }
+    public string Access { get; set; }
+    public List<string> Tags { get; set; }
+
+    public static PostDto MapToPostDto(Post post)
+    {
+        return new PostDto
+        {
+            Id = post.Id,
+            Title = post.Title,
+            Content = post.Content,
+            Author = UserPostDto.MapToUserPostDto(post.Author),
+            MainImageFilePath = post.Images.MainImage.ImageFilePath,
+            Likes = post.Likes,
+            CreatedAt = post.CreatedAt.ToString("yyyy-MM-dd"),
+            Access = post.Access,
+            Tags = post.Tags.Select(tag => tag.Name).ToList()
+        };
+    }
+    
+    public static List<PostDto> MapToPostDtoList(List<Post> posts)
+    {
+        return posts.Select(MapToPostDto).ToList();
+    }
+}
---
+++ backend/Back/Models/DTO/UserPostDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Back.Models.DTO;
+
+public class UserPostDto
+{
+    public string Username { get; set; }
+    public string FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    public string LastName { get; set; }
+    
+    public static UserPostDto MapToUserPostDto (User user)
+    {
+        return new UserPostDto
+        {
+            Username = user.Username,
+            FirstName = user.FirstName,
+            MiddleName = user.MiddleName,
+            LastName = user.LastName
+        };
+    }
+}
