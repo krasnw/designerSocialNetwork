b99a032c051287f3cd3debc099650b6a2c5c37ed
NonNoname1201 <dobrohorskyi.v@gmail.com>
2024-12-04 10:13:22 +0100

    feat: request creation

---
+++ backend/Back/Services/ChatService.cs
@@ -0,0 +1,63 @@
+﻿using Back.Models;
+using Npgsql;
+
+namespace Back.Services;
+
+public class ChatService
+{
+    private static DatabaseService _databaseService = DatabaseService.GetInstance();
+
+
+    // Under construction
+
+    public static bool SendRequest(string username, Chat.Request request)
+    {
+        var getUserIdQuery = @"
+    SELECT id FROM api_schema.user WHERE username = @Username";
+
+        NpgsqlConnection connection = null;
+        NpgsqlCommand command = null;
+
+        try
+        {
+            connection = _databaseService.GetConnection();
+
+            //sender ID
+            command = new NpgsqlCommand(getUserIdQuery, connection);
+            command.Parameters.AddWithValue("@Username", username);
+            var senderId = (int?)command.ExecuteScalar();
+            if (senderId == null) throw new Exception("Sender not found");
+
+            //receiver ID
+            command.Parameters.Clear();
+            command.Parameters.AddWithValue("@Username", request.Receiver);
+            var receiverId = (int?)command.ExecuteScalar();
+            if (receiverId == null) throw new Exception("Receiver not found");
+
+            //main request
+            var insertQuery = @"
+        INSERT INTO api_schema.request (buyer_id, seller_id, request_description, request_status)
+        VALUES (@SenderId, @ReceiverId, @Description, @Status::api_schema.request_status)";
+
+            command = new NpgsqlCommand(insertQuery, connection);
+            command.Parameters.AddWithValue("@SenderId", senderId);
+            command.Parameters.AddWithValue("@ReceiverId", receiverId);
+            command.Parameters.AddWithValue("@Description", request.Description);
+            command.Parameters.AddWithValue("@Status", "pending");
+
+            command.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while sending request: {ex.Message}");
+            return false;
+        }
+        finally
+        {
+            command?.Dispose();
+            connection?.Close();
+        }
+
+        return true;
+    }
+}
