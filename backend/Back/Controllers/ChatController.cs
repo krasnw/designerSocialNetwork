d8b719497fc27c2c237c031f35e01858fcab1f8c
NonNoname1201 <dobrohorskyi.v@gmail.com>
2024-12-04 10:14:03 +0100

    feat: mapping of request logic

---
+++ backend/Back/Controllers/ChatController.cs
@@ -0,0 +1,20 @@
+﻿using Back.Models;
+using Back.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Back.Controllers;
+
+public class ChatController : ControllerBase
+{
+    [Authorize]
+    [HttpPost("sendRequest")]
+    public IActionResult SendRequest([FromBody] Chat.Request request)
+    {
+        var uniqueName = User.Identity?.Name;
+        if (string.IsNullOrEmpty(uniqueName)) return Unauthorized("Blame the token, relog please");
+        
+        var chat = ChatService.SendRequest(uniqueName, request);
+        return chat != null ? Ok(chat) : BadRequest();
+    }
+}
