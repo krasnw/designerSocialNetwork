edd9de685ae11977e883f6120f6b8ab09707717b
dobrohor <dobrohorskyi.v@gmail.com>
2025-03-31 21:58:31 +0200

    addition of more configuration points
    Frontend is NOT WORKING

--- frontend-admin/src/services/constants.js
+++ frontend-admin/src/services/constants.js
@@ -1,15 +1,18 @@
-export const API_URL = "http://localhost:8082";
-export const imageDirectory = "http://localhost:8088/images/";
+let API_URL = `http://localhost:${import.meta.env.VITE_ADMINPANEL_PORT}`;
+let imageDirectory = `http://localhost:${
+	import.meta.env.VITE_SERVER_PORT
+}/images/`;
+export { API_URL, imageDirectory };
 
 export const formatNumber = (number) => {
-  if (number >= 1000000000) {
-    return (number / 1000000000).toFixed(1).replace(".", ",") + " B";
-  }
-  if (number >= 1000000) {
-    return (number / 1000000).toFixed(1).replace(".", ",") + " M";
-  }
-  if (number >= 1000) {
-    return (number / 1000).toFixed(1).replace(".", ",") + " K";
-  }
-  return number;
+	if (number >= 1000000000) {
+		return (number / 1000000000).toFixed(1).replace(".", ",") + " B";
+	}
+	if (number >= 1000000) {
+		return (number / 1000000).toFixed(1).replace(".", ",") + " M";
+	}
+	if (number >= 1000) {
+		return (number / 1000).toFixed(1).replace(".", ",") + " K";
+	}
+	return number;
 };
